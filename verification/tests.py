3894135ca046f44863bf9ee9e6e4f61b20742f6d
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-09-09 11:30:21 +1200

    Tests and template for referee

--- verification/tests.py
+++ verification/tests.py
@@ -1,36 +1,18 @@
-"""
-TESTS is a dict with all you tests.
-Keys for this will be categories' names.
-Each test is dict with
-    "input" -- input data for user function
-    "answer" -- your right answer
-    "explanation" -- not necessary key, it's using for additional info in animation.
-"""
-
-
 TESTS = {
-    "Basics": [
-        {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
-        },
-        {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
-        }
-    ],
-    "Extra": [
-        {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
-        },
-        {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
-        }
-    ]
+    "1st": {
+        "input": [5, 5, 0],
+        "goal": [7, 7]
+    },
+    "2nd": {
+        "input": [0, 0, 0],
+        "goal": [5, 6]
+    },
+    "3rd": {
+        "input": [0, 0, 0],
+        "goal": [9, 9]
+    },
+    "4th": {
+        "input": [7, 7, 0],
+        "goal": [2, 4]
+    },
 }
