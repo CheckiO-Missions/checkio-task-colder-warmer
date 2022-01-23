09e624546c20c139fe3333d08c00a4f3d26999e1
oduvan <a.lyabah@checkio.org>
2022-01-23 14:05:43 +0200

    json encode

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -39,7 +39,7 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
                 return false;
             }
             if (data.error) {
-                $content.find('.call').html('Fail: checkio(' + ext.JSON.encode(data.in) + ')');
+                $content.find('.call').html('Fail: checkio(' + ext.JSON.stringify(data.in) + ')');
                 $content.find('.output').html(data.error.replace(/\n/g, ","));
 
                 $content.find('.output').addClass('error');
@@ -61,17 +61,17 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
             //if you need additional info from tests (if exists)
             var explanation = data.ext["explanation"];
 
-            $content.find('.output').html('&nbsp;Your result:&nbsp;' + ext.JSON.encode(userResult));
+            $content.find('.output').html('&nbsp;Your result:&nbsp;' + ext.JSON.stringify(userResult));
 
             if (!result) {
-                $content.find('.call').html('Fail: checkio(' + ext.JSON.encode(checkioInput) + ')');
+                $content.find('.call').html('Fail: checkio(' + ext.JSON.stringify(checkioInput) + ')');
                 $content.find('.answer').html(result_addon);
                 $content.find('.answer').addClass('error');
                 $content.find('.output').addClass('error');
                 $content.find('.call').addClass('error');
             }
             else {
-                $content.find('.call').html('Pass: checkio(' + ext.JSON.encode(checkioInput) + ')');
+                $content.find('.call').html('Pass: checkio(' + ext.JSON.stringify(checkioInput) + ')');
                 $content.find('.answer').remove();
             }
 
